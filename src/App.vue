fb42e84a8f7f551d2eabad49739faac4dfbdf70f
Mio <gusw.0246.lpsyg@gmail.com>
2024-12-03 10:33:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data(){
+    return{
+      title: "Hola!! final project"
+    }
+  }
+  
+}
+</script>
+
+<style>
+  
+</style>
